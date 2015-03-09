0a9446345af7b40efe9c5d0fcb1692085456945c
Duncan Skinner <skinnerduncan@gmail.com>
2015-03-09 16:53:14 -0700

    use regex to groom input string of numbers

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 var refresh = function () {
-	var numString = $("#numbers").val();
+	var numString = $("#numbers").val().replace(/[" "]/g, "");
 	var $img = $("<img />").attr("src", "http://chart.apis.google.com/chart?cht=lc&chs=900x300&chd=t:" + numString + "&");
 	$img.load( function () {
         $("#graph").html($img);
